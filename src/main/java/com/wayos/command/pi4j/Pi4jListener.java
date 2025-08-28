2898a39990cb2fbf06d3186b7bf6844092724744
Wizarud <110596305+wizarud@users.noreply.github.com>
2025-08-28 22:08:35 +0700

    Initial commit

---
+++ src/main/java/com/wayos/command/pi4j/Pi4jListener.java
@@ -0,0 +1,57 @@
+package com.wayos.command.pi4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.annotation.WebListener;
+
+import com.wayos.Session;
+import com.wayos.command.wakeup.ExtensionSupportWakeupCommandNode;
+
+@WebListener
+public class Pi4jListener extends ExtensionSupportWakeupCommandNode.WebListener {
+
+	@Override
+	public void wakup(Session session) {
+		
+		session.commandList().add(new pi4jDigitalOutputCommandNode(session, new String[]{"pi4jOutput"}));
+		
+		System.out.println(session + " pi4j Commands ready..");
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		
+		super.contextInitialized(sce);
+		
+		/**
+		 * TODO: Check why too many load!!!!
+		 */
+		
+		Map<String, Map<String, String>> logicDesignerExtToolMap = 
+				(Map<String, Map<String, String>>)sce.getServletContext().getAttribute("logicDesignerExtToolMap");		
+		
+		Map<String, String> sampleEntity1Map = new HashMap<>();
+		sampleEntity1Map.put("tool-label", "Pi Out");
+		sampleEntity1Map.put("tool-color", "#D3D70D");
+		sampleEntity1Map.put("tool-tip", "Pi4J Entity!");
+		sampleEntity1Map.put("entity-resps", "["
+				+ "{"
+				+ "	txt: 'CMD',"
+				+ "	params: [{ parameterName: 'hook', value: 'pi4jOutput' }, { parameterName: 'params', value: 'pinNo hi|lo' }]"
+				+ "}"
+				+ "]");
+		
+		/**
+		 * DOM Id query pattern to apply colour
+		 * extCommand-<Hook>
+		 */
+		logicDesignerExtToolMap.put("extCommand-pi4jOutput", sampleEntity1Map);
+		
+		System.out.println("Loaded Sample Tools: " + sce.getServletContext().getAttribute("logicDesignerExtToolMap"));		
+		
+	}
+
+}
---
+++ src/main/java/com/wayos/command/pi4j/pi4jDigitalOutputCommandNode.java
@@ -0,0 +1,69 @@
+package com.wayos.command.pi4j;
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.wayos.MessageObject;
+import com.wayos.Session;
+import com.wayos.command.CommandNode;
+
+public class pi4jDigitalOutputCommandNode extends CommandNode {
+
+	public pi4jDigitalOutputCommandNode(Session session, String[] hooks) {
+		super(session, hooks);
+	}
+
+	@Override
+	public String execute(MessageObject messageObject) {
+		
+		String params = cleanHooksFrom(messageObject.toString());
+		
+		String [] tokens = params.split(" ");
+		
+		if (tokens.length!=2) {
+			
+			return "Invalid pi4j Parameters! Use <pin> <hi|lo>";
+		}
+				
+		int pin;
+		
+		try {
+			
+			pin = Integer.parseInt(tokens[0]);
+			
+		} catch (Exception e) {
+			
+			return e.getMessage() + ": Invalid pi4j Parameters! Use <pin> <hi|lo>";
+		}
+		
+		String action = tokens[1];
+		
+		String id = "pi4jId:" + pin;
+		
+		String name = id;
+		
+        Context pi4j = Pi4J.newAutoContext();
+        
+        DigitalOutput output = pi4j.create(DigitalOutput.newConfigBuilder(pi4j)
+                .id(id)
+                .name(name)
+                .address(pin) // mock pin address
+//                .provider("mock-digital-output") // IMPORTANT for mock mode
+                .build());
+        
+        if (action.equals("hi")) {
+        	
+            output.high();
+            
+    		return action;
+    		
+        } else if (action.equals("lo")) {
+        	
+    		return action;
+            
+        }
+		
+		return "Unknown action! Use hi|lo";
+	}
+
+}
